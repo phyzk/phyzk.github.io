c71e309dfa74d599df1910c6795b24831ac027ba
phyzk <zhoukdcn@outlook.com>
2023-09-11 08:02:30 +0800

    Site updated: 2023-09-11 08:02:29

--- injector.js
+++
@@ -1,6 +0,0 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>hexo.extend.injector.register('body_end', `
-  <div id="aplayer"></div>
-  <link defer rel="stylesheet" href="https://cdn.staticfile.org/aplayer/1.10.1/APlayer.min.css" />
-  <script src="https://cdn.staticfile.org/aplayer/1.10.1/APlayer.min.js"></script>
-  <script defer src="/js/aplayer.js"></script>
-`);
--- js/aplayer.js
+++ js/aplayer.js
@@ -17,8 +17,7 @@
             name: '花の塔',
             artist: 'さユり',
             url: 'http://music.163.com/song/media/outer/url?id=1956534872.mp3',
-            cover: 'https://p2.music.126.net/fS_5RbP_4qg-RYreqp2tGg==/109951167558017839.jpg?param=130y130',
-            lrc: 'https://moechun.fun/music/lrc/さユり - 花の塔.lrc'
+            cover: 'https://p2.music.126.net/fS_5RbP_4qg-RYreqp2tGg==/109951167558017839.jpg?param=130y130'
         }
         ]
       });
